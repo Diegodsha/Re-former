4bdca66743956d01802ae95e4fb9bf4d4d78e46f
Diego <diegodsha@hotmail.com>
2021-03-26 16:54:13 -0600

    Add linters and rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   def index
     @users = User.all
   end
@@ -9,8 +8,8 @@ class UsersController < ApplicationController
   end
 
   def create
-    #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
-     @user = User.new(user_params)
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
@@ -30,12 +29,11 @@ class UsersController < ApplicationController
     else
       render :edit
     end
-  
   end
 
   private
+
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
 end
