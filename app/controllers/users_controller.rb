15c6b302fe4b52037160cf368b43dbcb6878b1ed
Rashad-Muntar <muntartoure@gmail.com>
2021-03-26 21:59:08 +0000

    add edit and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,12 +12,26 @@ class UsersController < ApplicationController
     #@user = User.new(username: params[:username], email: params[:email], password: params[:password])
      @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path
+      redirect_to root_path
     else
       render :new
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  
+  end
 
   private
   def user_params
